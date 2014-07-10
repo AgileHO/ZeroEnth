4490b961203882d9b4d2c76f8921490dbd04c4b0
Kim Nobay <kimnobay@unknown-00-16-cb-a5-ca-d8.home>
2014-07-10 20:55:22 +0100

    JQuery dance

---
+++ map/jquery_testing.php
@@ -0,0 +1,159 @@
+<html>
+
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script src="jquery-ui.min.js"></script>
+<script src="ddslick.js"></script>
+<?php
+
+include("config.php");
+
+$locations = array();
+
+// run query
+//$result = mysql_query($conn,"SELECT * FROM locations");
+$result = mysqli_query($connMySQL,"SELECT * FROM locations");
+
+/* loop through the result set, for every row in the set create an array that represents the location, then push the location into
+the locations (NB plural) array */
+$locations=array();
+while($row = mysqli_fetch_array($result)) { 	
+  array_push($locations, $row['name']);
+ /* $location[1] = $row['lat'];
+  $location[2] = $row['long'];
+  $locations[] = $location; */
+}
+
+// take php array and turn it into json object
+$json = json_encode($locations);
+
+
+
+
+
+
+//inject the json object into js as a variable called locationsData.
+echo <<<END
+  <script type="text/javascript">
+	 var locationsData = $json;
+  </script>
+END;
+
+?>
+
+<script>
+
+var ddData = [
+    {
+        text: "Under 5",
+        value: 1,
+        description: "Janet street porter",
+        imageSrc: "http://static.guim.co.uk/sys-images/Media/Pix/pictures/2001/04/11/Street-Porter1.jpg"
+    },
+    {
+        text: "6 to 8",
+        value: 2,
+        description: "Jeremy Clarkson",
+        imageSrc: "http://static.guim.co.uk/sys-images/Media/Pix/pictures/2007/07/03/clarksonl.jpg"
+    },
+    {
+        text: "9",
+        value: 3,
+        description: "Ornella Mutti",
+        imageSrc: "http://imalbum.aufeminin.com/album/D20080716/446715_GBQ6ISIC2IOAXXECJYX2JQOYN2YIVY_156_H105203_S.jpg"
+    },
+    {
+        text: "10",
+        value: 4,
+        description: "Pele",
+        imageSrc: "http://i.imgur.com/aEGiK.png"
+    }
+];
+
+ $(
+ 	function() 
+ 	{
+ 		var availableTags = locationsData;
+		$( "#where" ).autocomplete({source: availableTags});
+		$( "#optionspanel" ).accordion();
+		
+		$( "#clickme" ).click(function() {
+		$( "#optionspanel" ).toggle( "slow", function() {
+		});
+		});
+		
+		$('#myDropdown').ddslick({
+    	data:ddData,
+    	width:300,
+    	selectText: "How fit?",
+    	imagePosition:"right",
+    	onSelected: function(selectedData){
+    }   
+});
+		
+		
+				
+	}
+);
+
+
+
+
+
+
+
+</script>
+
+
+
+</head>
+
+
+
+
+<body>
+
+<h1>
+Jquery testing
+</h1>
+
+
+<form>
+<label for="where">Where are you?</label>
+
+
+
+
+
+
+<input type="text" id="where">
+
+<div id="clickme">show/hide</div>
+<div id="optionspanel">
+
+<p>
+Here are some options
+</p>
+
+	 <div id="myDropdown"></div>
+
+
+</div>
+
+<input type="submit" />
+
+
+
+
+
+</form>
+
+
+
+
+
+</body>
+
+
+</html>
